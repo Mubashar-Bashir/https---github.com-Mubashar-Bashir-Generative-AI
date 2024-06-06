d734b4a7ad59253ab2ee6616c78c2bc1fde7ca95
Mubashar-Bashir <mubasharbashir003@gmail.com>
2024-06-06 12:48:32 +0500

    crud operations in ai mart

--- mart/product-service-aimart/app/main.py
+++ mart/product-service-aimart/app/main.py
@@ -1,14 +1,18 @@
+from typing import Annotated, List
 from fastapi import FastAPI, Depends, HTTPException, status
 from contextlib import asynccontextmanager
-from app.db_c_e_t_session import create_db_and_tables
+from app.db_c_e_t_session import create_db_and_tables,get_session
+import asyncio
+from sqlmodel import Session, SQLModel
 
-
+from app.models.product_model import Product, ProductUpdate
+from app.crud.crud_product import add_new_product, get_all_products, get_product_by_id, delete_product_by_id, update_product_by_id
 
 # Async context manager for application lifespan events
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Creating tables for product-service-aimart..")
-    # create_db_and_tables()  # Create database tables
+    create_db_and_tables()  # Create database tables
     yield  # Application startup
 
 # Create FastAPI app with custom lifespan and metadata
@@ -29,3 +33,14 @@ app = FastAPI(
 @app.get("/")
 def read_root():
     return {"Welcome": "welcome to my mobi product-service-aimart local computer"}
+
+# Create a product
+@app.post("/products", response_model=Product)
+async def create_new_product(product: Product, session: Annotated[Session, Depends(get_session)]):
+    new_product = add_new_product(product, session)
+    return new_product
+
+@app.get("/products", response_model = List[Product])
+async def get_products(session=get_session()):
+    all_products = get_all_products(session)
+    return all_products
